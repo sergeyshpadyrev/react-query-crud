35ba94dbd11b8692d1f5cbb4c80fe834a243edcd
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2024-11-18 12:31:38 +0400

    fix: make client any

--- src/provider/types.ts
+++ src/provider/types.ts
@@ -1,6 +1,4 @@
-import { QueryClient } from '@tanstack/react-query';
-
 export interface QueryCrudClientProviderProps {
     children: React.ReactNode;
-    client: QueryClient;
+    client: any;
 }
